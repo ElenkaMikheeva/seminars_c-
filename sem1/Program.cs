c7e7bb0d47f96db53b95ddb76f0df31f69f035fd
Helena Veles <helena.veles@mail.ru>
2023-09-19 11:36:24 +0300

    добавила семинар1

---
+++ sem1/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+System.Console.WriteLine("Input number: "); 
+int number = Convert.ToInt32(Console.ReadLine()); 
+int index = - number; 
+while(index < number + 1)
+ {
+ System.Console.WriteLine($" {index}, "); 
+	index++; 
+} 
+
+System.Console.WriteLine("Введите число 3-х значное число: "); 
+int number1 = Convert.ToInt32(Console.ReadLine()); 
+System.Console.WriteLine($"{number1} -> {number1 %10}"); 
+
+
+ System.Console.WriteLine("Введите число 3-х значное число: ");
+  int number2 = Convert.ToInt32(Console.ReadLine()); 
+ if(number2>=100 && number2 <=999)
+  { 
+ System.Console.Write($"{number2} -> {number2 %10}"); 
+ }
+  else
+  { 
+System.Console.WriteLine("Вы ввели не коректное число");
+  } 
+
